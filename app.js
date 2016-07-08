3619977bba6d6b0c70617bb193b70554917865d3
amontoya49 <andresmhkun@gmail.com>
2016-07-08 07:16:42 -0500

    firts commit
    firts commit

---
+++ app.js
@@ -0,0 +1,80 @@
+/**
+ * Created by andres.montoya on 4/07/2016.
+ */
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var User = require("./models/users").User;
+var session = require("express-session");
+var app = express();
+
+
+
+app.use("/public", express.static('public'));
+app.use(bodyParser.json());     //usado para peticiones applications/json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+    secret: "123kdlkfhbsdfbsdv3",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.set("view engine","jade");
+
+app.get("/", function (req, res) {
+    console.log(req.session.user_id);
+    res.render("index");
+});
+
+
+app.get("/signup", function (req, res) {
+    User.find(function (err, doc) {
+        console.log(doc);
+        res.render("signup");
+    });
+    
+    
+
+});
+
+app.get("/login", function (req, res) {
+        res.render("login");
+
+});
+
+app.post("/users", function (req, res) {
+    //console.log("password " + req.body.password);
+    //console.log("email " + req.body.email);
+    var user = new User({email: req.body.email, password: req.body.password, password_confirmation: req.body.password_confirmation, username: req.body.username});
+    // print by console the virtual attrib
+    //console.log(user.password_confirmation);
+
+    //function (err) es un callback, se ejecutara una vez se intente guardar en la base de datos.
+
+    /*user.save(function (err) {
+    if(err)
+    {
+        console.log(err);
+    }
+        res.send("data saved");
+    });*/
+    ///save using promises from ECMAScript 6
+    user.save().then(function (us) {
+        res.send("Data Saved");
+    }, function (err) {
+        if(err){
+            console.log(String(err));
+            res.send("not Saved");
+        }
+
+    });
+});
+
+app.post("/sessions", function (req, res) {
+    User.findOne({email:req.body.email, password:req.body.password}, function (err, user) {
+        req.session.user_id = user._id;
+        res.send("Hello world");
+    });
+});
+
+app.listen(8080);
---
+++ models/users.js
@@ -0,0 +1,34 @@
+/**
+ * Created by andres.montoya on 5/07/2016.
+ */
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var email_match = [/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalidate e-mail"];
+var sex_values = ["M","F"];
+var pass_validation = {
+    validator: function (p) {
+        return this.password_confirmation == p;
+    },
+    message: "The passwords are differents"
+}
+mongoose.connect("mongodb://localhost/photos");
+
+var user_schema = new Schema({
+    name: String,
+    last_name: String,
+    username: {type:String, required:true, maxLength:[50,"Username too long"]},
+    password: {type:String, minlength:[8, "pass too short"], validate: pass_validation},
+    age: {type:Number, min:[5,"age less than 5"], max:[100, "age greater than 100"]},
+    email: {type:String, required:"the mail is not optional", match: email_match},
+    date_birth: Date,
+    sex: {type:String, enum:{values: sex_values, message: "invalidate sex"}}
+});
+
+user_schema.virtual("password_confirmation").get(function () {
+    return this.p_c;
+}).set(function (password) {
+    this.p_c = password;
+});
+
+var User = mongoose.model("User", user_schema);
+module.exports.User = User;
